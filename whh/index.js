4870f01cf7469904eea3d1f768ba4d26bbb937e1
whh <739250179@qq.com>
2017-06-29 17:56:27 +0800

    zuoye

---
+++ whh/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/6/29 0029.
+ */
+
+window.onload=function(){
+    var img=document.getElementsByTagName('img')[0];
+    img.onclick=function(){
+        alert("这是个镁铝！！！")
+    }
+}
